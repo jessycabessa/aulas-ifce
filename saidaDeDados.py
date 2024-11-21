b6e012c30ceb723605216c6298f24c5efda58f14
Jéssyca Bessa <80965667+jessycabessa@users.noreply.github.com>
2024-11-21 14:04:00 -0300

    Add files via upload

---
+++ saidaDeDados.py
@@ -0,0 +1,45 @@
+#Printando uma mensagem na tela
+#print("Imprimindo alguma mensagem")
+#print('Imprimindo mensagem 2')
+
+#Imprimir uma variável inteira
+#var = 20
+#print(var)
+
+#Imprimir uma variável str
+#var = "xxyyzz"
+#print(var)
+
+# Imprimir múltiplas variáveis
+#nome  = "João"
+#idade = 25
+#print(nome, idade)
+
+# Imprimir múltiplas variáveis com separador
+#nome  = "João"
+#idade = 25
+#print(nome, idade, sep='~')
+
+#Impressão com dados diferentes
+nome  = "Sátiro"
+idade = 15
+print("Nome do aluno:", nome)
+print("Idade do aluno:", idade)
+# f-strings que é uma formatação mais moderna
+# para incluir variáveis dentro de strings
+print(f"O nome do aluno é {nome} e a idade é {idade}")
+
+#concatenando strings (msg+variável)
+nome = "Wagner"
+print ("O nome dele é " +  nome)
+
+#Exercício
+
+#1)Um programa que solicite o nome do usuário imprima uma saudação personalizada.
+
+#2)Receber 3 notas e imprimir a média das notas
+
+#3)Imprima contagem regressiva de 10 a 0
+
+#4)Escolher um numero e imprimir a tabuada de multiplicação 
+
