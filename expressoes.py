80e8e171d4c60e3f8c8e3cf18fb584bc7417fbfe
Jéssyca Bessa <80965667+jessycabessa@users.noreply.github.com>
2024-11-28 14:19:15 -0300

    Add files via upload

---
+++ expressoes.py
@@ -0,0 +1,16 @@
+### Conversor Milha - KM
+#dist_milhas = float(input("Digite uma distância em milhas: "))
+#dist_km  = 1.6*dist_milhas
+#print(f"A distância em km é: {dist_km}")
+
+##Equação do segundo Grau
+
+#a = int(input("Digite o coeficiente a: "))
+#b = int(input("Digite o coeficiente b: "))
+#c = int(input("Digite o coeficiente c: "))
+
+#delta = (b**2) - (4*a*c)
+#x1 = (-b + (delta**0.5))/(2*a)
+#x2 = (-b - (delta**0.5))/(2*a)
+
+#print(f"As raízes da equação são: {x1} e {x2}")
