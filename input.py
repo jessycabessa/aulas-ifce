45e4df5f20683e2be4e6526f02d0ea8f87e99f49
Jéssyca Bessa <80965667+jessycabessa@users.noreply.github.com>
2024-11-28 14:18:09 -0300

    Add files via upload

---
+++ input.py
@@ -0,0 +1,46 @@
+#Receber uma string
+#nome = input("Digite o seu nome: ")
+#print(f"O seu nome é {nome}")
+
+#Convertendo o dado para outro tipo
+#n1 = int(input("Digite um numero: "))
+#n2 = int(input("Digite um numero: "))
+#soma = n1+n2
+#print(f"A soma é: {soma}")
+
+# Cadastro de pessoas
+# Nome, idade, peso, altura, endereço e cpf
+#nome  = input("Digite seu nome: ")
+#idade = int(input("Digite sua idade: "))
+#peso  = float(input("Digite seu peso: "))
+#altura = float(input("Digite sua altura: "))
+#end    = input("Digite seu endereço: ")
+#cpf    = input("Digite seu cpf: ")
+
+#print("------------------------------------------")
+#print(f"Nome: {nome} - CPF: {cpf}")
+#print(f"Endereço: {end}")
+#print(f"Idade: {idade} - Peso: {peso} - Altura:{altura}")
+
+# Calculando a idade
+# Neste exemplo, o programa pede ao usuário seu ano de nascimento
+# e calcula a idade com base no ano atual.
+#ano = int(input("Digite o seu ano de nascimento: "))
+#idade = 2024-ano
+#print (f"A idade é: {idade}")
+
+#O programa pede ao usuário para informar a largura e 
+#altura de um retângulo, e então
+#calcula a área.
+
+#alt = int(input("Digite uma altura: "))
+#larg = int(input("Digite uma largura: "))
+#area = larg*alt
+#print("A área do retângulo é: ", area,"m2")
+
+#input com split
+
+num1, num2 = input("Digite dois números: ").split()
+num1 = int(num1)
+num2 = int(num2)
+print(num1+num2)
